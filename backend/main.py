cbd5a858199a75e61b29ea59ddd946fec3d12a3b
Arsaki2 <arsaki2@mail.ru>
2026-08-17 12:57:15 +0300

    🔧 Gunicorn без порта

--- backend/main.py
+++ backend/main.py
@@ -219,8 +219,3 @@ def index():
 </form>
 </body></html>"""
 
-if __name__ == "__main__":
-    if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)), debug=False)
