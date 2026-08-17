96612847c6edb95a44198d891814887a63ddd5b0
Arsaki2 <arsaki2@mail.ru>
2026-08-17 13:14:53 +0300

    🔧 app.run() без параметров

--- backend/main.py
+++ backend/main.py
@@ -70,4 +70,4 @@ def chat():
     return jsonify({"success": True, "type": "knowledge", "answer": "Ничего не найдено"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    app.run()
