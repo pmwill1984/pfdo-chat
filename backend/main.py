15c443594cfd90d070bc7da5254d3761d451f1f6
Arsaki2 <arsaki2@mail.ru>
2026-08-17 12:43:37 +0300

    🔧 Исправлен sys.path

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ import re
 import random
 import sys
 
-sys.path.append(os.path.dirname(__file__))
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), ""))
 from knowledge_base import KNOWLEDGE_BASE, DIRECTIONS, REGIONS, find_answer
 
 app = Flask(__name__)
